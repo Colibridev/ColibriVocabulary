36d77515d632c0ecedf80bac5d639361c6232f64
Alexey Misiulia <alexeymisiulia@gmail.com>
2019-01-28 10:34:28 +0300

    Add Room

--- app/src/main/java/com/devcolibri/translator/activity/WordListActivity.java
+++ app/src/main/java/com/devcolibri/translator/activity/WordListActivity.java
@@ -1,10 +1,12 @@
 package com.devcolibri.translator.activity;
 
-import java.util.Arrays;
 import java.util.List;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
@@ -14,7 +16,9 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 import com.devcolibri.translator.R;
+import com.devcolibri.translator.VocabularyApp;
 import com.devcolibri.translator.adapter.WordAdapter;
+import com.devcolibri.translator.db.AppDatabase;
 import com.devcolibri.translator.entity.Word;
 
 public class WordListActivity extends AppCompatActivity {
@@ -24,17 +28,41 @@ public class WordListActivity extends AppCompatActivity {
     private FloatingActionButton addWordButton;
     private WordAdapter wordAdapter;
 
+    private AppDatabase database;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initViews();
+
+        database = VocabularyApp.getDatabase();
+
+        loadWords();
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private void loadWords() {
+        new AsyncTask<Void, Void, List<Word>>() {
+            @Override
+            protected List<Word> doInBackground(Void... voids) {
+                return database.getPersonDao().getAllWords();
+            }
+
+            @Override
+            protected void onPostExecute(List<Word> words) {
+                wordAdapter.setWords(words);
+            }
+        }.execute();
+    }
+
+    private void initViews() {
         recyclerView = findViewById(R.id.words_recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         wordAdapter = new WordAdapter();
         recyclerView.setAdapter(wordAdapter);
         recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayout.VERTICAL));
-        wordAdapter.setWords(getWords());
 
         addWordButton = findViewById(R.id.add_word_button);
         addWordButton.setOnClickListener(new View.OnClickListener() {
@@ -46,13 +74,12 @@ public class WordListActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-    private List<Word> getWords() {
-        return Arrays.asList(
-            new Word("Mum", "Мама"),
-            new Word("Dad", "Папа"),
-            new Word("Brother", "Брат"),
-            new Word("Sister", "Сестра")
-        );
+        if(requestCode == ADD_WORD_CODE && resultCode == RESULT_OK) {
+            loadWords();
+        }
     }
 }
--- app/src/main/java/com/devcolibri/translator/entity/Word.java
+++ app/src/main/java/com/devcolibri/translator/entity/Word.java
@@ -1,6 +1,13 @@
 package com.devcolibri.translator.entity;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity
 public class Word {
+    @PrimaryKey
+    @NonNull
     private String word;
     private String translation;
 
